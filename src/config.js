bc6fd6f93204fadb332b3369dd70439e37e169d9
Eric Nguyen <enguyen89141@gmail.com>
2019-12-19 21:34:59 -0800

    had to change DB to DATABASE for heroku push

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:dunder@localhost/allowance',
-    TEST_DB_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:dunder@localhost/allowance-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:dunder@localhost/allowance',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:dunder@localhost/allowance-test',
     JWT_SECRET: process.env.JWT_SECRET || 'little-brother-name-christopher',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '10m'
 }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const knex = require('knex')
 const app = require('./app')
 
-const { PORT, DB_URL } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 /** MAKE SURE TO CHANGE TEST_DB BACK TO DB_URL or DATABASE URL and IN POSTGRATOR/ENV/CONFIG FILE BEFORE DEPLOYMENT*/
 
 const db = knex({
   client: 'pg',
-  connection: DB_URL,
+  connection: DATABASE_URL,
 })
 
 app.set('db', db)
